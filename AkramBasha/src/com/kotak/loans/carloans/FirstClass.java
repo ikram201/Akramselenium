8555342e7265b94ec477ddbbd2eb9a4f8e6efa4c
ikram201 <ggk.vikram@gmail.com>
2022-10-20 19:13:20 +0530

    selenium20-10-2022
    selenium20-10-2022

---
+++ AkramBasha/src/com/kotak/loans/carloans/FirstClass.java
@@ -0,0 +1,17 @@
+package com.kotak.loans.carloans;
+
+import javax.management.BadStringOperationException;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		sopln
+
+	} public static void main(String[] args) {
+		
+		BadStringOperationException
+		sysout
+		
+	}
+}
---
+++ AkramBasha/src/module-info.java
@@ -0,0 +1,2 @@
+module AkramBasha {
+}
